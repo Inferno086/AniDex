2d700e7f27463a1032ae0f46d4651cd1e966f04f
inferno086350 <as086350@gmail.com>
2022-11-05 14:42:45 +0530

    Added Start Button functionality in the app. Now after pressing start button, when the image of the character is clicked, a new character is shown.

--- app/src/main/java/com/inferno/anidex/CharacterFragment.kt
+++ app/src/main/java/com/inferno/anidex/CharacterFragment.kt
@@ -2,15 +2,18 @@ package com.inferno.anidex
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.inferno.anidex.databinding.FragmentCharacterBinding
+import kotlin.random.Random
 
 class CharacterFragment : Fragment(R.layout.fragment_character) {
 
     lateinit var binding : FragmentCharacterBinding
+    val totalCharacters : Int = 3
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding = FragmentCharacterBinding.bind(view)
+        binding = FragmentCharacterBinding.bind(view)  //Alternate way to bind a fragment
 
         // List of Characters
         val characters = listOf<Character>(
@@ -18,12 +21,23 @@ class CharacterFragment : Fragment(R.layout.fragment_character) {
             Character("Naruto Uzumaki", getString(R.string.naruto_description), R.drawable.naruto),
             Character(name = "Shinichi Izumi", getString(R.string.shinichi_discription), R.drawable.shinichi)
         )
-        updateCharacter(characters[0])
+
+        displayRandomCharacterFromList(characters, totalCharacters) // For the first time
+
+        //onClickListener for the image
+        binding.characterImage.setOnClickListener { view ->
+            displayRandomCharacterFromList(characters, totalCharacters)
+        }
     }
 
     private fun updateCharacter(character: Character) {
         binding.characterName.text = character.name
         binding.characterDescription.text = character.description
         binding.characterImage.setImageResource(character.image)
+    }
+
+    private fun displayRandomCharacterFromList(list : List<Character>, totalNoOfCharacters : Int) {
+        val randomNumber : Int = Random.nextInt(0,totalNoOfCharacters)
+        updateCharacter(list[randomNumber])
     }
 }

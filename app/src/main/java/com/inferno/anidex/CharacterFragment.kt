83839ffd44a764945b17a84576f2bd76af09e5ac
inferno086350 <as086350@gmail.com>
2022-11-08 12:00:12 +0530

    Added the share button in CharacterFragment.kt in the action bar using MenuHost and MenuProvider. The share button does nothing at the moment.

--- app/src/main/java/com/inferno/anidex/CharacterFragment.kt
+++ app/src/main/java/com/inferno/anidex/CharacterFragment.kt
@@ -1,9 +1,15 @@
 package com.inferno.anidex
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import com.inferno.anidex.databinding.FragmentCharacterBinding
 import kotlin.random.Random
 
@@ -25,6 +31,10 @@ class CharacterFragment : Fragment(R.layout.fragment_character) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding = FragmentCharacterBinding.bind(view)  //Alternate way to bind a fragment
 
+        // For share button
+        val menuHost : MenuHost = requireActivity()
+        shareButtonInflater(menuHost)
+
         // List of Characters
         val characters = listOf<Character>(
             Character("Eikichi Onizuka", getString(R.string.onizuka_description), R.drawable.onizuka),
@@ -42,14 +52,34 @@ class CharacterFragment : Fragment(R.layout.fragment_character) {
         }
     }
 
+    // This function updates the character displayed when called
     private fun updateCharacter(character: Character) {
         binding.characterName.text = character.name
         binding.characterDescription.text = character.description
         binding.characterImage.setImageResource(character.image)
     }
 
+    // This function uses updateCharacter() to display a random character
+    // This is called when the user clicks the ImageView
     private fun displayRandomCharacterFromList(list : List<Character>, totalNoOfCharacters : Int) {
         val randomNumber : Int = Random.nextInt(0,totalNoOfCharacters)
         updateCharacter(list[randomNumber])
+    }
+
+    // This function is for inflating the share button
+    // This can be done directly in onViewCreated() as well
+    // It has been done for simplicity in onViewCreated()
+    // setHasOptionsMenu(true) has been deprecated and this is the replacement
+    private fun shareButtonInflater(menuHost : MenuHost) {
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // Add menu items here
+                menuInflater.inflate(R.menu.share_button, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED )
     }
 }
